a410113cab88f8b9526e84cce5148c5d7543ade7
msaito <peterpansymdromer@gmail.com>
2014-03-02 12:07:57 +0900

    Revert "finish user signup2"
    This reverts commit 31a913136d9bad78500d491a591b8c1b721b589b.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params) 
     if @user.save
-	    flash[:success] = "Welcome to the Sample APp!"
+	    flash[:success] = "Welcom to the Sample APp!"
 	    redirect_to @user
     else
 	    render 'new'
