f9b171c9999dd49a441bf596ae6098574f50055e
msaito <peterpansymdromer@gmail.com>
2014-03-10 02:37:54 +0900

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params) 
     if @user.save
-	    flash[:success] = "Welcom to the Sample APp!"
+	    sign_in @user
+	    flash[:success] = "Welcome to the Sample APp!"
 	    redirect_to @user
     else
 	    render 'new'
